39c2f501a04f1f986384930a7df661e7e3319e1a
Nicolas Thouchkaieff <thouch_n@epitech.eu>
2018-03-29 16:18:08 +0900

    gameTags now object

--- tags/lol.js
+++ tags/lol.js
@@ -44,20 +44,56 @@ const setting = {
     }
 };
 
-module.exports = [{
-    id: 'LOL__RANKED_SOLO_SR__TIER', // Tag ID
-    gameID: 'lol', // game ID
-    category: 'Ranked Solo Summoner\'s Rift', // Category name
-    categorySmall: 'Ranked Solo SR', // Category small name
-    name: 'Tier', // Tag name
-    nameSmall: 'Tier', // Tag small name
-    size: { // Tag sizes
-        default: 10, // default is required !!
-        short: 6
+module.exports = {
+    LOL__RANKED_SOLO_SR__TIER: {
+        id: 'LOL__RANKED_SOLO_SR__TIER', // gameTag ID : {GAME_ID}__{CATEGORY_SMALL}_{NAME_SMALL}
+        gameID: 'lol', // game ID
+        category: 'Ranked Solo Summoner\'s Rift', // Category name
+        categorySmall: 'Ranked Solo SR', // Category small name
+        name: 'Tier', // Tag name
+        nameSmall: 'Tier', // Tag small name
+        size: { // Tag sizes
+            default: 10, // default is required !!
+            short: 6
+        },
+        fieldSettings: {
+            account: setting.account, // existing setting
+            size: setting.size, // existing setting
+            format: setting.format // existing setting
+        }
     },
-    fieldSettings: {
-        account: setting.account, // existing setting
-        size: setting.size, // existing setting
-        format: setting.format // existing setting
+    LOL__RANKED_SOLO_SR__TIER2: {
+        id: 'LOL__RANKED_SOLO_SR__TIER2', // Tag ID
+        gameID: 'lol', // game ID
+        category: 'Ranked Solo Summoner\'s Rift22', // Category name
+        categorySmall: 'Ranked Solo SR', // Category small name
+        name: 'Tier', // Tag name
+        nameSmall: 'Tier', // Tag small name
+        size: { // Tag sizes
+            default: 10, // default is required !!
+            short: 6
+        },
+        fieldSettings: {
+            account: setting.account, // existing setting
+            size: setting.size, // existing setting
+            format: setting.format // existing setting
+        }
+    },
+    LOL__RANKED_SOLO_SR__TIER3: {
+        id: 'LOL__RANKED_SOLO_SR__TIER3', // Tag ID
+        gameID: 'lol', // game ID
+        category: 'Ranked Solo Summoner\'s Rift22', // Category name
+        categorySmall: 'Ranked Solo SR', // Category small name
+        name: 'Tier', // Tag name
+        nameSmall: 'Tier', // Tag small name
+        size: { // Tag sizes
+            default: 10, // default is required !!
+            short: 6
+        },
+        fieldSettings: {
+            account: setting.account, // existing setting
+            size: setting.size, // existing setting
+            format: setting.format // existing setting
+        }
     }
-}];
+};
