d15b166920ac78fa9dd6be6c759e0160a96be6f1
Nicolas Thouchkaieff <thouch_n@epitech.eu>
2018-03-31 17:52:05 +0900

    0 tags speedrun

---
+++ tags/speedrun.js
@@ -0,0 +1,13 @@
+const setting = {
+    account: {
+        type: 'account', // select
+        tooltip: false,
+        input: false, // already handled by the component
+        label: { // required
+            en: 'Account',
+            fr: 'Compte'
+        }
+    }
+};
+
+module.exports = {};
