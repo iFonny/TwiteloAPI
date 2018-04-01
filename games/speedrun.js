e7ca178d75065637c1ce4f19f01ec0c642091b26
Nicolas Thouchkaieff <thouch_n@epitech.eu>
2018-04-01 21:57:21 +0900

    improvement game fonctions js file

--- games/speedrun.js
+++ games/speedrun.js
@@ -1,27 +1,41 @@
 /*
-** Get & resolve game account ID
-*/
-module.exports.getAccountID = (username) => {
+ ** Get & resolve game account ID
+ **
+ ** Params: 
+ ** - settings (required: account settings
+ **
+ ** Return: 
+ ** - string: account ID in game
+ */
+module.exports.getAccountID = (settings) => {
 
     return new Promise((resolve, reject) => {
-        
+
         // TODO
-        resolve('ifonny');
+        resolve('46741395');
 
     });
 
 };
 
 /*
-** Update & resolve game data for account
-*/
-module.exports.updateAccountGameData = (account) => {
+ ** Update db & resolve game data for accounts
+ **
+ ** Params : 
+ ** - accounts (required): Array of accounts
+ ** - object (optional): Object with tags to update (all if undefined) like : 
+ **            {LOL__RANKED_SOLO_SR__TIER: true, LOL__RANKED_SOLO_SR__LP: true}
+ **
+ ** Return: 
+ ** - array: array with game accounts data
+ */
+module.exports.updateAccountGameData = (accounts, tags) => {
 
     return new Promise((resolve, reject) => {
-        
+
         // TODO: Get stats
         // TODO: Update db
-        resolve('{Toutes les stats/rank de luser}');
+        resolve('[{Toutes les stats/rank de luser}]');
 
     });
 
