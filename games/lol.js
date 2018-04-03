22ac4f45ec8eaf564ca1d689dbfe351abca10bc9
Nicolas Thouchkaieff <thouch_n@epitech.eu>
2018-04-03 18:41:12 +0900

    game ratelimits config

--- games/lol.js
+++ games/lol.js
@@ -4,5 +4,10 @@ module.exports = {
     name: 'League Of Legends',
     icon: '/public/images/game/lol/icon.png',
     image: '/public/images/game/lol/image.png',
-    color: '#f9d380'
+    color: '#f9d380',
+    ratelimit: {
+        request: 300,
+        every: 10, // 300 requests / 10 seconds
+        total: 600 // 18 000 requests / 10 minutes
+    }
 };
--- games/speedrun.js
+++ games/speedrun.js
@@ -4,5 +4,10 @@ module.exports = {
     name: 'Speedrun.com',
     icon: '/public/images/game/speedrun/icon.png',
     image: '/public/images/game/speedrun/image.png',
-    color: '#d68548'
+    color: '#d68548',
+    ratelimit: {
+        request: 30,
+        every: 60, // 30 requests / 1 minute
+        total: 1800 // 900 requests / 30 minutes
+    }
 };
